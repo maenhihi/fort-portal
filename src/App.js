bd32b9e4a5d189648590cb755ce1f8b555ffb948
maenhihi <maenhihi@outlook.com>
2024-09-18 10:58:04 +1000

    lol

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import MapPage from "./Pages/MapPage";
+import HomePage from "./Pages/HomePage";
+import { LogInPage } from "./Pages/LogInPage";
+import ValidLogin from "./Pages/ValidLogin";
+import InvalidLogin from "./Pages/InvalidLogin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/MapPage" element={<MapPage />} />
+            <Route path="/LogInPage" element={<LogInPage />} />
+            <Route path="/ValidLogin" element={<ValidLogin />} />
+            <Route path="/InvalidLogin" element={<InvalidLogin />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
